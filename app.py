31e61a8667b1c1e24f2d7b99bee49764f004d314
bhavanis8 <bhavanis2143@gmail.com>
2026-04-24 21:07:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Disaster Processing System Running"
+    return '''
+    <h2>Disaster Processing System</h2>
+    <a href="/run">Click here to Run Project</a>
+    '''
 
 @app.route('/run')
-def run_script():
-    result = subprocess.getoutput("python main.py")
-    return f"<pre>{result}</pre>"
+def run_project():
+    output = subprocess.getoutput("python main.py")
+    return f"<pre>{output}</pre>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
